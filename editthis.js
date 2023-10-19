3a76cc7948a154ea8d662966cbd87f2215b7f766
Facundo Chiappa <124144540+FacundoChiappa@users.noreply.github.com>
2023-10-19 15:04:11 -0300

    Update editthis.js

--- editthis.js
+++ editthis.js
@@ -1,29 +1,29 @@
 var site = {
       /* Your name */
-    name : 'Adithya Pai',
-    job: 'Front-End Developer',
+    name : 'Facundo Chiappa',
+    job: 'FullStack Developer',
        /* This decription is used both in meta tag and about page */
-    description: `Hi there! I am Adithya Pai B from Mangalore, India.
-    I am currently pursuing Bachelor of Engineering in Computer Science.
+    description: `Hi there! I am Facundo Chiappa from Buenos Aires, Argentina.
+    I am currently pursuing Technical Programmer at UTN.
     I ❤️ to design Websites, UI/UX, and Photography.`,
     /* Change your skills and title accodingly */
-    skills_title: ['Web Development', 'Design','Programming'],
-    skills: [['HTML', 'CSS', 'JS', 'ReactJS','SCSS'], ['Photoshop', 'XD','Figma'],['C','C++','Python3', 'Javascript']],
-    skillp:[[100, 80 ,100, 60, 80], [50, 60, 50], [70,80,90,90]],
+    skills_title: ['Web Development', 'Design','Programming', 'Data Base'],
+    skills: [['HTML', 'CSS', 'JS', 'ReactJS','SCSS', 'Angular'], ['Photoshop', 'XD','Figma'],['C','C++','Java', 'Python3', 'Javascript', 'MySQL', 'Ruby', 'Rust']],
+    skillp:[[100, 80 ,100, 80, 80, 100], [50, 60, 70], [70,80,90,90,90,80,70,100]],
     social: {
         /* Add your social media links here */
-        github: 'https://github.com/adithyapaib',
-        twitter: 'https://twitter.com/adithyapaib',
-        linkedin: 'https://www.linkedin.com/in/adithyapaib/',
-        mail: 'mailto: email@gmail.com',
-        instagram: 'https://instagram.com'
+        github: 'https://github.com/FacundoChiappa',
+        twitter: 'https://twitter.com/facuu_chiappaa',
+        linkedin: 'https://www.linkedin.com/in/facundo-chiappa-15a9b7235/',
+        mail: 'mailto: facuchiappa2011@hotmail.com',
+        instagram: 'https://instagram.com/facuu_chiappa'
     },
-    form: 'https://formsubmit.co/email@gmail.com',
+    form: 'https://formsubmit.co/facuchiappa2011@hotmail.com',
     /* DONT FOR GET TO ACTIVATE UR EMAIL AFTER SETTING UP THE FORM TO RECEIVE EMAILS */
     projects: [
-        ["https://google.com", 'https://github.com/adithyapaib/portfolio-template',"https://github.com/adithyapaib/portfolio-template", "https://github.com/adithyapaib/portfolio-template"],
-         ['Title', 'Title', "Title", "Title"],
-        ['Decription', 'Decription', "Decription", "Decription"],
+        ["https://facundochiappa.github.io/Netflix-Card-Ex/", "https://facundochiappa.github.io/Tarjeta3D/","https://facundochiappa.github.io/creador-de-password/", "https://facundochiappa.github.io/landing-adobe/"],
+         ['Netflix Card Example', 'Travel 3D Card', 'Random Password Generator', "Landing Page Adobe"],
+        ['This is a Netflix Serie Card example created by myself', 'This is a 3D Product card with animations created for a travel company', "This is a Random Password Generator created with scripts with the purpose to make yoy password safetier", "This is an example Landing Page with the theme of Adobe"],
         
     ],
     //Web site color scheme
